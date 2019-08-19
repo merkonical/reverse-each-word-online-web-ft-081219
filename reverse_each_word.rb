2ec634667bfb029761204e53362316e0f9fe07e2
Maverick Adolfo <merkonical@gmail.com>
2019-08-19 05:33:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,6 +7,5 @@ def reverse_each_word(sentence1)
 end 
 
 def reverse_each_word(sentence1)
-  def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+  sentence1.split.collect {|word| word.reverse}.join(" ")
 end
